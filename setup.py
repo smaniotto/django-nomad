e6843c57659b5fc190e5e4c3b7143948ecf32e86
Bernardo Smaniotto <be.smaniotto@gmail.com>
2017-06-19 23:12:13 -0300

    Add django dependency to setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,18 @@ from setuptools import setup, find_packages
 setup(
     author='Bernardo Smaniotto',
     author_email='bernardo@cheesecakelabs.com',
-    classifiers=['Development Status :: 1 - Planning',
-                 'Environment :: Web Environment',
-                 'Framework :: Django',
-                 'Intended Audience :: Developers',
-                 'Operating System :: OS Independent',
-                 'Programming Language :: Python :: 3',
-                 'Topic :: Utilities'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities'
+    ],
     description='Easily handle migrations in dev mode',
     include_package_data=True,
-    install_requires=[],
+    install_requires=['django'],
     name='django-dev-migrations',
     packages=find_packages(),
     version='0.0.1'
