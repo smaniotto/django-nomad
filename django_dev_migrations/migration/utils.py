1deebd9382ce6c9860f5fe1ac2ba520917fbf64d
Bernardo Smaniotto <be.smaniotto@gmail.com>
2017-06-21 10:10:02 -0300

    Add functions to check if migrations is applied

--- django_dev_migrations/migration/utils.py
+++ django_dev_migrations/migration/utils.py
@@ -1,7 +1,11 @@
 import re
 
 from django.conf import settings
-settings.configure()
+from django.db import connections, DEFAULT_DB_ALIAS
+from django.db.migrations.recorder import MigrationRecorder
+
+
+MIGRATION_MODULE_TEMPLATE = '[\w\-\.]*/{}/[\w\-\. ]+\.py$'
 
 
 def filter_migration_files(files_list):
@@ -20,9 +24,9 @@ def filter_migration_files(files_list):
     )
 
     # Get list of migration folders
-    default_migration_folder = '^[\w\-\.]*/migrations/[\w\-\. ]+\.py$'
+    default_migration_folder = MIGRATION_MODULE_TEMPLATE.format('migrations')
     other_migration_folders = [
-        '^[\w\-\.]*/{}.py$'.format(module.replace('.', '/'))
+        MIGRATION_MODULE_TEMPLATE.format(module.replace('.', '/'))
         for _, module in settings.MIGRATION_MODULES.items()
     ]
 
@@ -31,3 +35,36 @@ def filter_migration_files(files_list):
     generic_migration_folder_regex = re.compile('|'.join(generic_migration_folder_list))
 
     return list(filter(lambda f: generic_migration_folder_regex.match(f), clean_files_list))
+
+
+def extract_migration_info_from_path(path):
+    """
+    Extract the migration app and name from a migration path, with format:
+        [.../]<app>/<migrations-module>/<migration-name>.py
+
+    Args:
+    path (string): migration file path.
+
+    Returns:
+    app, name (tuple): migration information
+    """
+    path_split = path.split('/')
+    app = path_split[-3]
+    name = path_split[-1].replace('.py', '')
+    return app, name
+
+
+def is_migration_applied(app, name):
+    """
+    Check on migrations database table if the given migration is applied to the database.
+
+    Args:
+    app (string): django app to which the migration belongs.
+    name (string): migration name.
+
+    Returns:
+    bool: True, if the migration is applied.
+    """
+    db_connection = connections[DEFAULT_DB_ALIAS]
+    recorder = MigrationRecorder(db_connection)
+    return recorder.record_applied(app=app, name=name)
